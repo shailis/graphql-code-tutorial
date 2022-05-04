2633c81fc1b1c84697d15bca83e2b445cbfca45f
shaili <shailis@zignuts.com>
2022-05-04 15:49:13 +0530

    GraphQL API With NodeJS and Apollo Server | GraphQL Course For Beginners Ep. 3

---
+++ FakeData.js
@@ -0,0 +1,39 @@
+const userList = [
+    {
+        id: 1,
+        name: "John",
+        username: "john",
+        age: 20,
+        nationality: "Canada"
+    },
+    {
+        id: 2,
+        name: "Pedro",
+        username: "pedro",
+        age: 20,
+        nationality: "Brazil"
+    },
+    {
+        id: 3,
+        name: "Sarah",
+        username: "cameron",
+        age: 25,
+        nationality: "United States"
+    },
+    {
+        id: 4,
+        name: "Rafe",
+        username: "rafe123",
+        age: 60,
+        nationality: "Germany"
+    },
+    {
+        id: 5,
+        name: "Kelly",
+        username: "kelly1998",
+        age: 5,
+        nationality: "Chile"
+    }
+];
+
+module.exports = { userList };
