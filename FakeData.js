f38f63daae80956fe9f3098bb12e0ff72a2d2c44
shaili <shailis@zignuts.com>
2022-05-04 16:39:31 +0530

    GraphQL Resolvers | GraphQL Course For Beginners Ep. 4

--- FakeData.js
+++ FakeData.js
@@ -4,36 +4,88 @@ const userList = [
         name: "John",
         username: "john",
         age: 20,
-        nationality: "Canada"
+        nationality: "CANADA",
+        friends: [
+            {
+                id: 2,
+                name: "Pedro",
+                username: "pedro",
+                age: 20,
+                nationality: "BRAZIL"
+            },
+            {
+                id: 5,
+                name: "Kelly",
+                username: "kelly1998",
+                age: 5,
+                nationality: "CHILE"
+            }
+        ]
     },
     {
         id: 2,
         name: "Pedro",
         username: "pedro",
         age: 20,
-        nationality: "Brazil"
+        nationality: "BRAZIL"
     },
     {
         id: 3,
         name: "Sarah",
         username: "cameron",
         age: 25,
-        nationality: "United States"
+        nationality: "INDIA",
+        friends: [
+            {
+                id: 2,
+                name: "Pedro",
+                username: "pedro",
+                age: 20,
+                nationality: "BRAZIL"
+            } 
+        ]
     },
     {
         id: 4,
         name: "Rafe",
         username: "rafe123",
         age: 60,
-        nationality: "Germany"
+        nationality: "GERMANY"
     },
     {
         id: 5,
         name: "Kelly",
         username: "kelly1998",
         age: 5,
-        nationality: "Chile"
+        nationality: "CHILE"
     }
 ];
 
-module.exports = { userList };
+const movieList = [
+    {
+        id: 1,
+        name: "Avengers Endgame",
+        yearOfPublication: 2019,
+        isInTheaters: true
+    },
+    {
+        id: 2,
+        name: "Interstellar",
+        yearOfPublication: 2007,
+        isInTheaters: true
+    },
+    {
+        id: 3,
+        name: "SuperBad",
+        yearOfPublication: 2009,
+        isInTheaters: true
+    },
+    {
+        id: 4,
+        name: "The Smalls",
+        yearOfPublication: 2035,
+        isInTheaters: false
+    }
+];
+
+module.exports = { userList, movieList };
--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,11 +1,33 @@
-const { userList } = require("../FakeData");
+const { userList, movieList } = require("../FakeData");
+const _ = require("lodash");
 
 const resolvers = {
     Query: {
-        users() {
+        // USER RESOLVERS
+        users: () => {
             return userList;
+        },
+        user: (parent, args) => {
+            const id = args.id;
+            const user = _.find(userList, { id: Number(id) });
+            return user;
+        },
+
+        // MOVIE RESOLVERS
+        movies: () => {
+            return movieList;
+        },
+        movie: (parent, args) => {
+            const name = args.name;
+            const movie = _.find(movieList, { name });
+            return movie;
+        }
+    },
+    User: {
+        favoriteMovies: () => {
+            return _.filter(movieList, (movie) => movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010);
         }
     }
-}
+};
 
 module.exports = { resolvers };
